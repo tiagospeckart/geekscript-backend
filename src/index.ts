e28e6532f30ac834db941de5c83513e3800ca959
Thiago Almeida <tcalmeida85@gmail.com>
2023-02-22 18:05:49 -0300

    feat: enable CORS package to handle cross-origin requests

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import { mySqlConection } from "./database";
 import "dotenv/config";
 import BaseRoutes from "./infra/BaseRoutes"
 import handleError from "./Middlewares/handleError";
+import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(BaseRoutes);
 app.use(handleError);
 
