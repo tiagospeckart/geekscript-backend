9d4f22f0fdc0aa0e6478e963c8063d6127d27b3a
Thiago Almeida <tcalmeida85@gmail.com>
2023-02-22 10:55:52 -0300

    build: adds validation functionality to User routes

--- src/modules/User/routes.ts
+++ src/modules/User/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import controller from "./controller";
+import createValidation from "../../Validations/User/create";
+import updateValidation from "../../Validations/User/update";
 
 const routes = Router();
 
-routes.post("/user", controller.create);
+routes.post("/user", createValidation, controller.create);
 routes.get("/user", controller.findAll);
 routes.get("/user/:id", controller.findOne);
-routes.put("/user/:id", controller.update);
+routes.put("/user/:id", updateValidation, controller.update);
 routes.delete("/user/:id", controller.delete);
 
 export default routes;
