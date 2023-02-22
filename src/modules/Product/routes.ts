e7cfe1c55307f033ce7f535919ae5efbb7ab5fc1
Thiago Almeida <tcalmeida85@gmail.com>
2023-02-22 19:54:20 -0300

    build: adds validation functionality to Product routes

--- src/modules/Product/routes.ts
+++ src/modules/Product/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import controller from "./controller";
+import createValidation from "../../Validations/Product/create";
+import updateValidation from "../../Validations/Product/update";
 
 const routes = Router();
 
-routes.post("/product", controller.create);
+routes.post("/product", createValidation, controller.create);
 routes.get("/product", controller.findAll);
 routes.get("/product/:id", controller.findOne);
-routes.put("/product/:id", controller.update);
+routes.put("/product/:id",  updateValidation, controller.update);
 routes.delete("/product/:id", controller.delete);
 
 export default routes;
