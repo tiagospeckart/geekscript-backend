3799fb25f689dedab0d946c02bac302b2eecba2f
Thiago Almeida <tcalmeida85@gmail.com>
2023-02-23 12:47:02 -0300

    update: add login route

--- src/infra/BaseRoutes/index.ts
+++ src/infra/BaseRoutes/index.ts
@@ -1,12 +1,14 @@
-import { Router } from "express";
-import userRoutes from "../../modules/User/routes";
-import productRoutes from "../../modules/Product/routes";
-import categoryRoutes from "../../modules/Category/routes";
+import { Router } from 'express';
+import userRoutes from '../../modules/User/routes';
+import productRoutes from '../../modules/Product/routes';
+import categoryRoutes from '../../modules/Category/routes';
+import loginRoute from '../../modules/Login/route';
 
 const routes = Router();
 
 routes.use(userRoutes);
 routes.use(productRoutes);
 routes.use(categoryRoutes);
+routes.use(loginRoute);
 
 export default routes;
