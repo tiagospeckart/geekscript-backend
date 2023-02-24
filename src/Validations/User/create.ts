cf0a836d56f27397536330915481584d0350e1c3
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-02-24 19:21:16 -0300

    update: remove minimum chars required

--- src/Validations/User/create.ts
+++ src/Validations/User/create.ts
@@ -4,7 +4,7 @@ export default validate({
   body: Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+    password: Joi.string().required(),
     isAdm: Joi.boolean().required(),
   }),
 });
