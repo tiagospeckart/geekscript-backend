bb5aeeac8b2a491a473abe7f68345d86c1e2a67a
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-02-24 18:57:55 -0300

    build: create Purchase model

---
+++ src/models/Purchase.ts
@@ -0,0 +1,46 @@
+import {DataTypes, CreationOptional, InferAttributes, InferCreationAttributes, Model, ForeignKey} from "sequelize";
+import { mySqlConection } from "../database";
+import User from "./User";
+
+const dbConnection = mySqlConection.getInstance();
+
+
+interface Purchase extends Model<InferAttributes<Purchase>, InferCreationAttributes<Purchase>> {
+  id_purchase: CreationOptional<number>;
+  user_id: ForeignKey<number>;
+  total: number;
+  createdAt: CreationOptional<Date>;
+  updatedAt: CreationOptional<Date>;
+}
+
+const Purchase = dbConnection.define<Purchase>(
+  "Purchase",
+  {
+    id_purchase: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+    total: {
+      type: DataTypes.DECIMAL,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    tableName: "purchase",
+  }
+);
+
+export default Purchase;
