1a3d1e817d49c0b106a511511d2ec001b0e52050
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-02-24 11:13:12 -0300

    feat: implements user authentication on category, product && user routes

--- src/modules/Category/routes.ts
+++ src/modules/Category/routes.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import controller from "./controller";
 import createValidation from "../../Validations/Category/create";
 import updateValidation from "../../Validations/Category/update";
+import userAuthentication from "../../Middlewares/authentication";
 
 const routes = Router();
 
-routes.post("/category", createValidation, controller.create);
+routes.post("/category", userAuthentication, createValidation, controller.create);
 routes.get("/category", controller.findAll);
 routes.get("/category/:id", controller.findOne);
-routes.put("/category/:id", updateValidation, controller.update);
-routes.delete("/category/:id", controller.delete);
+routes.put("/category/:id", userAuthentication, updateValidation, controller.update);
+routes.delete("/category/:id", userAuthentication, controller.delete);
 
 export default routes;
--- src/modules/User/routes.ts
+++ src/modules/User/routes.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import controller from "./controller";
 import createValidation from "../../Validations/User/create";
 import updateValidation from "../../Validations/User/update";
+import userAuthentication from "../../Middlewares/authentication";
 
 const routes = Router();
 
 routes.post("/user", createValidation, controller.create);
-routes.get("/user", controller.findAll);
-routes.get("/user/:id", controller.findOne);
-routes.put("/user/:id", updateValidation, controller.update);
-routes.delete("/user/:id", controller.delete);
+routes.get("/user", userAuthentication, controller.findAll);
+routes.get("/user/:id", userAuthentication, controller.findOne);
+routes.put("/user/:id", userAuthentication, updateValidation, controller.update);
+routes.delete("/user/:id", userAuthentication, controller.delete);
 
 export default routes;
