9f86810b0705a5bcac2e9e92344c954ac122a249
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-02-24 18:51:59 -0300

    update: BaseRoutes with Purchase routes

--- src/infra/BaseRoutes/index.ts
+++ src/infra/BaseRoutes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import userRoutes from '../../modules/User/routes';
 import productRoutes from '../../modules/Product/routes';
 import categoryRoutes from '../../modules/Category/routes';
+import purchaseRoutes from '../../modules/Purchase/routes';
 import loginRoute from '../../modules/Login/route';
 
 const routes = Router();
@@ -9,6 +10,7 @@ const routes = Router();
 routes.use(userRoutes);
 routes.use(productRoutes);
 routes.use(categoryRoutes);
+routes.use(purchaseRoutes);
 routes.use(loginRoute);
 
 export default routes;
