cb9810aacff8e514b678fabf17cfe656d4adc584
tiagospeckart <tiagospeckart@gmail.com>
2023-02-25 13:54:19 -0300

    docs: basic swagger implementation

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import BaseRoutes from "./infra/BaseRoutes"
 import handleError from "./Middlewares/handleError";
 import cors from "cors";
 import { mySqlConection } from "./database";
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument  from "./api-docs/swagger.json";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use ('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(BaseRoutes);
 app.use(handleError);
 
