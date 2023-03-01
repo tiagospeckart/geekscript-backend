29b0ea9d3d67e15869bb6f92f4b06fe59553738a
tiagospeckart <tiagospeckart@gmail.com>
2023-02-28 23:25:20 -0300

    fix: controllers refactor

---
+++ src/controllers/CategoryController.ts
@@ -0,0 +1,101 @@
+import { Request, Response } from "express";
+import { Category } from "../models";
+
+const categoryController = {
+  async create(req: Request, res: Response) {
+    try {
+      const { name } = req.body;
+
+      const checkCategory = await Category.count({ where: { name } });
+      if (checkCategory) {
+        return res.status(409).json("categoria já existente");
+      }
+
+      const newCategory = await Category.create({
+        name,
+      });
+      return res.status(201).json(newCategory);
+    } catch {
+      return res.status(400).json("Não foi possível realizar o cadastro");
+    }
+  },
+
+  async findAll(req: Request, res: Response) {
+    try {
+      const findCategories = await Category.findAll();
+      return res.status(200).json(findCategories);
+    } catch (error) {
+      return res.status(500).json("Não foi possível realizar a ação");
+    }
+  },
+
+  async findOne(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      let findCategory = await Category.findByPk(id);
+
+      if (!findCategory) {
+        return res.status(404).json("Id não encontrado");
+      }
+
+      findCategory = await Category.findByPk(id, {
+        attributes: {
+          exclude: ["password"],
+        },
+      });
+      return res.status(200).json(findCategory);
+    } catch {
+      return res.status(500).json("Não foi possível realizar a ação");
+    }
+  },
+
+  async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const { name } = req.body;
+
+      const checkCategory = await Category.findByPk(id);
+      if (!checkCategory) {
+        return res.status(404).json("Id não encontrado");
+      }
+
+      await Category.update(
+        {
+          name,
+        },
+        {
+          where: {
+            id_category: id,
+          },
+        }
+      );
+
+      const showCategory = await Category.findByPk(id);
+      return res.status(200).json(showCategory);
+    } catch (error) {
+      return res.status(500).json("Não foi possível atualizar o cadastro");
+    }
+  },
+
+  async delete(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      let deleteCategory = await Category.findByPk(id);
+      if (!deleteCategory) {
+        return res.status(404).json("Id não encontrado");
+      }
+      await Category.destroy({
+        where: {
+          id_category: id,
+        },
+      });
+      return res.status(204).json();
+    } catch (error) {
+      return res.status(500).json("Não foi possível realizar a ação");
+    }
+  },
+};
+
+export default categoryController;
---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from 'express';
+import User from '../models/User';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import key from '../configs/jwtKey';
+
+export default class loginController {
+  static login = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+    const { email, password } = req.body;
+
+    const dataUser = await User.findOne({ where: { email } });
+
+    if (!dataUser) {
+      return res
+        .status(401)
+        .json('Invalid e-mail or password, please try again');
+    }
+
+    if (!bcrypt.compareSync(password, dataUser.password)) {
+      return res
+        .status(401)
+        .json('Invalid e-mail or password, please try again');
+    }
+
+    try {
+      const token = jwt.sign(
+        {
+          id_user: dataUser.id_user,
+          name: dataUser.name,
+          email: dataUser.email,
+          scope: dataUser.scope,
+        },
+        key.privateKey,
+        { expiresIn: '30 days' }
+      );
+
+      // Criar uma função para selecionar o id e retornar na resposta
+      
+      return res.status(200).json({ id: dataUser.id_user, token: token });
+    } catch (error) {
+      return res.status(500).json('Não foi possível realizar a ação');
+    }
+  };
+}
