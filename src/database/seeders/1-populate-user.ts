5b6b0fe1c498eeeaf60cd3c1d6d86f7776af123a
tiagospeckart <tiagospeckart@gmail.com>
2023-03-02 22:02:13 -0300

    fix: migration

--- src/database/seeders/1-populate-user.ts
+++ src/database/seeders/1-populate-user.ts
@@ -5,7 +5,7 @@ export default {
         return queryInterface.bulkInsert('user', [{
           name: "admin",
           email: "admin@admin.com",
-          password: "$2a$10$JictknZGMcThCtg5Xld07u62DE.9of6q22uIWljDayU0E4k7NTB4q",
+          password: "$2a$10$i4kCP2hgjlVcQWoVUVmFAu3y/BlR0YYmohQGXOeFztyRw.NnOupuq",
           scope: 'admin',
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
@@ -13,7 +13,7 @@ export default {
         {
           name: "client",
           email: "client@client.com",
-          password: "$2a$10$A4WbUIXOlvOk0wITH98mM.geSbBIN2o/xx.MvqLHjCBJ.3xQeRJDi",
+          password: "$2a$10$q9fKFxDd/mmyGLZOQd.5xu6J35MEyeLjceYkJSjAUqlFHR22.Cnfq",
           scope: 'client',
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
