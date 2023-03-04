c80f69aa6899c62fbc39d307291b8faa3182c62d
tiagospeckart <tiagospeckart@gmail.com>
2023-03-04 01:14:51 -0300

    fix: migration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -17,12 +17,6 @@ router.use('/product', product);
 router.use('/category',category);
 router.use('/purchase', purchase);
 router.use('/login', login);
-<<<<<<< HEAD
 router.use('/checkout', checkout);
-router.use('/api_doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-=======
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
->>>>>>> main
 
 export default router
