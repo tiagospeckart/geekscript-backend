74f67a125ad16d70a78c7501ab12f381b64ffd71
tiagospeckart <tiagospeckart@gmail.com>
2023-03-06 19:56:43 -0300

    feat: Checkout controller working

--- src/controllers/CheckoutController.ts
+++ src/controllers/CheckoutController.ts
@@ -5,34 +5,25 @@ import jwt from 'jsonwebtoken';
 export default class checkoutController {
   static create = async (req: Request, res: Response): Promise<Response> => {
     try {
-
-      // --- CONSTANTES --- //
       const { purchaseIdList, total } = req.body;
       const authHeader: any = req.headers.authorization;
       const getToken = authHeader.split(' ')[1];
       const verifyToken: any = jwt.verify(getToken, process.env.SECRET as string);
       const user_id = verifyToken.id_user;
 
-      console.log(user_id) // OK
-      console.log(purchaseIdList) // OK
-      console.log(total) // OK
-
       const newPurchase: Purchase = await Purchase.create({
         user_id,
         total
+      }).catch((error) => {
+        console.error(error);
+        throw new Error('Failed to create new purchase');
       });
-      console.log(newPurchase) // Não tá inserindo direito, por que será??
 
       const checkoutArray = purchaseIdList.map((currElement:number) => PurchaseProduct.create({
         purchase_id: newPurchase.id_purchase,
         product_id: currElement,
       }))
 
-      
-      
-      
-      
-      
       return res.status(201).json(checkoutArray);
     } catch {
       return res.status(400).json('Não foi possível realizar a compra');
