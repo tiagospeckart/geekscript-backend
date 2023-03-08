8054d9c171adc521f851ae495cc9f1c1c6f9ce7d
tiagospeckart <tiagospeckart@gmail.com>
2023-03-08 00:08:49 -0300

    fix: migration text

--- src/database/seeders/4-populate-discount.ts
+++ src/database/seeders/4-populate-discount.ts
@@ -5,13 +5,13 @@ export default {
         return queryInterface.bulkInsert('discount', [{
           id_discount: 1,
           value: 15,
-          name: 'Cupom 15', 
+          name: 'cupom15', 
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
         },{
           id_discount: 2,
           value: 50,
-          name: 'Cupom 55.55', 
+          name: 'cupom50', 
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
         }
