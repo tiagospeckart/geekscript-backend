3e097c00394e5fe142dd01349a1642fd1d498a3e
tiagospeckart <tiagospeckart@gmail.com>
2023-03-09 22:23:30 -0300

    fix: seeders discount adaptation

--- src/database/seeders/5-populate-purchase.ts
+++ src/database/seeders/5-populate-purchase.ts
@@ -5,11 +5,13 @@ export default {
         return queryInterface.bulkInsert('purchase', [{
           user_id: 1,
           total: 393.73,
+          discount_id: null,
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
         },{
           user_id: 2,
           total: 558.85,
+          discount_id: 1,
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
         }
