eeb0e35b48f41e81e601089f410c6911dc561b96
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-03-09 22:52:19 -0300

    fix: checkout alternative version

--- src/controllers/CheckoutController.ts
+++ src/controllers/CheckoutController.ts
@@ -1,29 +1,46 @@
 import { Request, Response } from "express";
-import { Purchase, PurchaseProduct } from '../models';
+import { Discount, Purchase, PurchaseProduct } from '../models';
 import jwt from 'jsonwebtoken';
 import MESSAGE from "../constants/messages";
+import checkoutArray from '../helpers/createPurchaseProduct';
 
 export default class checkoutController {
   static create = async (req: Request, res: Response): Promise<Response> => {
     try {
-      const { purchaseIdList, total } = req.body;
+      const { purchaseIdList, total, discountName } = req.body;
       const authHeader: any = req.headers.authorization;
       const getToken = authHeader.split(' ')[1];
       const verifyToken: any = jwt.verify(getToken, process.env.SECRET as string);
       const user_id = verifyToken.id_user;
 
+      const checkDiscountName = await Discount.findOne({ where: { name: discountName } });
+      
+      if (checkDiscountName) {
+        const catchDiscountId = checkDiscountName.id_discount
+         const newPurchase = await Purchase.create({
+          user_id,
+          total,
+          discount_id: catchDiscountId
+        }).catch((error) => {
+          console.error(error);
+          throw new Error(MESSAGE.ERROR.CHECKOUT_REG);
+        });
+        checkoutArray(purchaseIdList, newPurchase)
+      }
+
       const newPurchase: Purchase = await Purchase.create({
         user_id,
-        total
+        total,
       }).catch((error) => {
         console.error(error);
         throw new Error(MESSAGE.ERROR.CHECKOUT_REG);
       });
 
-      const checkoutArray = purchaseIdList.map((currElement:number) => PurchaseProduct.create({
+      checkoutArray(purchaseIdList, newPurchase)
+      /* const checkoutArray = purchaseIdList.map((currElement:number) => PurchaseProduct.create({
         purchase_id: newPurchase.id_purchase,
         product_id: currElement,
-      }))
+      })) */
 
       return res.status(201).json(checkoutArray);
     } catch {
