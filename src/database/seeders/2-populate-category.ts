76776c1062111710cf740ee9ae068992e3a6b445
tiagospeckart <tiagospeckart@gmail.com>
2023-03-12 01:24:05 -0300

    refac: categories fixed for query purposes

--- src/database/seeders/2-populate-category.ts
+++ src/database/seeders/2-populate-category.ts
@@ -3,12 +3,19 @@ import { QueryInterface, DataTypes } from "sequelize"
 export default {
     up: (queryInterface: QueryInterface) =>{
         return queryInterface.bulkInsert('category', [{
-          name: "Jogos de Tabuleiro",
+          name: "boardgames",
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
-        },
-        {
-          name: "Video Game",
+        },{
+          name: "videogames",
+          createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
+          updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
+        },{
+          name: "clothes",
+          createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
+          updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
+        },{
+          name: "accessories",
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
         }])
