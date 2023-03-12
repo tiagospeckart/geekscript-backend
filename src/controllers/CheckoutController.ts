e43aa6cbb89ecf3c2239e5f6132a619a4ea8ee59
tiagospeckart <tiagospeckart@gmail.com>
2023-03-11 22:27:11 -0300

    refac: importing new CheckoutController

--- src/controllers/CheckoutController.ts
+++ src/controllers/CheckoutController.ts
@@ -1,66 +1,75 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { Discount, Purchase, PurchaseProduct } from '../models';
 import jwt from 'jsonwebtoken';
-import MESSAGE from '../constants/messages';
-import checkoutArray from '../helpers/createPurchaseProduct';
+import MESSAGE from "../constants/messages";
 
-export default class checkoutController {
-  static create = async (req: Request, res: Response): Promise<Response> => {
+async function getDiscountId(discountName: string): Promise<number | undefined> {
+  if (!discountName) {
+    return undefined;
+  }
+  const discount = await Discount.findOne({ where: { name: discountName } });
+
+  if (!discount) {
+    throw new Error('Invalid discount name');
+  }
+  return discount.id_discount;
+}
+
+async function getNewPurchaseTotal(purchaseTotal: number, discountId?: number): Promise<number> {
+    if (!discountId) {
+    return purchaseTotal;
+  }
+  const discount = await Discount.findByPk(discountId);
+  if (!discount) {
+    throw new Error('Invalid discount name');
+  }
+  const newPurchaseTotal = purchaseTotal - discount.value;
+  return newPurchaseTotal;
+}
+
+async function createNewPurchase(userId: number, purchaseTotal: Promise<number>, discountId?: number): Promise<Purchase> {
+  const resolvedPurchaseTotal = await purchaseTotal;
+  return Purchase.create({ user_id: userId, total: resolvedPurchaseTotal, discount_id: discountId });
+}
+
+async function addPurchaseProducts(purchaseId: number, purchaseIdList: number[]): Promise<PurchaseProduct[]> {
+  const createPurchaseProduct = async (productId: number) => {
+    return PurchaseProduct.create({
+      purchase_id: purchaseId,
+      product_id: productId,
+    });
+  };
+  const promises = purchaseIdList.map(createPurchaseProduct);
+
+  return Promise.all(promises);
+}
+
+export default class CheckoutController {
+  static async create(req: Request, res: Response): Promise<Response> {
     try {
-      const { purchaseIdList, total, discountName } = req.body;
-      const authHeader: any = req.headers.authorization;
-      const getToken = authHeader.split(' ')[1];
-      const verifyToken: any = jwt.verify(
-        getToken,
-        process.env.SECRET as string
-      );
-      const user_id = verifyToken.id_user;
+      const { purchaseIdList, discountName, purchaseTotal } = req.body;
+      const authHeader = req.headers.authorization as string;
+      const token = authHeader.split(' ')[1];
+      const { id_user } = jwt.verify(token, process.env.SECRET as string) as { id_user: number };
 
-      const checkDiscountName = await Discount.findOne({
-        where: { name: discountName },
-      });
+      const discountId = await getDiscountId(discountName);
+      const newPurchaseTotal = getNewPurchaseTotal(purchaseTotal, discountId)
 
-      if (checkDiscountName) {
-        const catchDiscountId = checkDiscountName.id_discount;
-        const newPurchase = await Purchase.create({
-          user_id,
-          total,
-          discount_id: catchDiscountId,
-        }).catch((error) => {
-          console.error(error);
-          throw new Error(MESSAGE.ERROR.CHECKOUT_REG);
-        });
-        checkoutArray(purchaseIdList, newPurchase);
-      }
-
-      const newPurchase: Purchase = await Purchase.create({
-        user_id,
-        total,
-      }).catch((error) => {
-        throw new Error(MESSAGE.ERROR.CHECKOUT_REG);
-      });
-
-      checkoutArray(purchaseIdList, newPurchase);
-      /* const checkoutArray = purchaseIdList.map((currElement:number) => PurchaseProduct.create({
-        purchase_id: newPurchase.id_purchase,
-        product_id: currElement,
-      })) */
-
-      const finishedPurchase = await PurchaseProduct.findAll({
-        attributes: [
-          'id_purchase_product',
-          'product_id',
-          'purchase_id',
-          'createdAt',
-        ],
-        where: {
-          purchase_id: newPurchase.id_purchase - 1, // correção do valor
+      const newPurchase = await createNewPurchase(id_user, newPurchaseTotal, discountId);
+      await addPurchaseProducts(newPurchase.id_purchase, purchaseIdList);
+      const populatedPurchase = await PurchaseProduct.findAll({
+        attributes: ['id_purchase_product', 'product_id', 'purchase_id', 'createdAt'],
+        where: { purchase_id: newPurchase.id_purchase },
+        include: {
+          model: Purchase,
+          attributes: ['user_id', 'discount_id', 'total'],
         },
       });
 
-      return res.status(201).json(finishedPurchase);
-    } catch {
+      return res.status(201).json(populatedPurchase);
+    } catch (error) {
+      console.error(error);
       return res.status(400).json(MESSAGE.ERROR.CHECKOUT);
     }
-  };
+  }
 }
