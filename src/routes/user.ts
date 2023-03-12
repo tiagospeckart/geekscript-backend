a84b5eae5cb0f3ea707cf71aeea7f54d2ac813a4
tiagospeckart <tiagospeckart@gmail.com>
2023-03-12 10:20:53 -0300

    feat: profile routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,13 +3,21 @@ import UserController from "../controllers/UserController";
 import createValidation from "../validations/User/create";
 import updateValidation from "../validations/User/update";
 import userAuthentication from "../middlewares/authentication";
+import adminVerification from "../middlewares/adminVerification";
+import PurchaseController from "../controllers/PurchaseController";
 
 const router = Router();
 
 router.post("/register", createValidation, UserController.create);
-router.get("/admin", userAuthentication, UserController.findAll);
-router.get("/:id", userAuthentication, UserController.findOne);
-router.put("/:id", userAuthentication, updateValidation, UserController.update);
-router.delete("/:id", userAuthentication, UserController.delete);
+// client routes
+router.get("/profile", userAuthentication, UserController.findMyUser);
+router.get('/profile/purchases', PurchaseController.findAllUserPurchase);
+router.put("/profile/edit", userAuthentication, UserController.updateMyUser);
+router.delete("/profile/delete", userAuthentication, UserController.deleteMyUser);
+// admin routes
+router.get("/admin", userAuthentication, adminVerification, UserController.findAll);
+router.get("/admin/:id", userAuthentication, adminVerification, UserController.findOne);
+router.put("/admin/:id", userAuthentication, adminVerification, updateValidation, UserController.update);
+router.delete("/admin/:id", userAuthentication, adminVerification, UserController.delete);
 
 export default router;
