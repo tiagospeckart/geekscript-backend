4c26ee234bcf76bcf265e4bb03129b66d6bb6ea1
tiagospeckart <tiagospeckart@gmail.com>
2023-03-13 20:55:44 -0300

    fix: migrations

--- src/configs/configSequelize.ts
+++ src/configs/configSequelize.ts
@@ -1,12 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
-console.log(process.env.DB_NAME)
-console.log(process.env.DB_HOST)
-console.log(process.env.DB_DIALECT)
-
 module.exports = {
   "development": {
     "username": process.env.DB_USER,
--- src/database/seeders/4-populate-discount.ts
+++ src/database/seeders/4-populate-discount.ts
@@ -3,13 +3,11 @@ import { QueryInterface, DataTypes } from "sequelize"
 export default {
     up: (queryInterface: QueryInterface) =>{
         return queryInterface.bulkInsert('discount', [{
-          id_discount: 1,
           value: 15,
           name: 'coupon15', 
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
           updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' ')
         },{
-          id_discount: 2,
           value: 50,
           name: 'coupon50', 
           createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
